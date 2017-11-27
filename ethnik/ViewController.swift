9ed2e6b299e3c3b0c9cf82592c3e4b2b47b01020
Simon Ibssa <ibssasimon@gmail.com>
2017-11-26 17:17:43 -0800

    pushing to firebase

--- ethnik/ViewController.swift
+++ ethnik/ViewController.swift
@@ -18,17 +18,60 @@ class ViewController: UIViewController {
         
         let ref = Database.database().reference()
         
-        let child = ref.childByAutoId()
+        let Mediterranean = ref.childByAutoId()
         
-        let dataType = [
-            "Restaurant Name": "restaurant",
-            "Food Type": "food",
-            "Location": "Stub",
+        let jaffa = Mediterranean.childByAutoId()
+        let fattoush = Mediterranean.childByAutoId()
+        let pitaPit = Mediterranean.childByAutoId()
+        let petra = Mediterranean.childByAutoId()
+        
+        
+        //initialization of values for restaurants
+        let jaffaData = [
+            "Restaurant Name": "Jaffa Cafe",
+            "Food Type": "Mediterranean",
+            "Longitute": "-120.65708899999998",
+            "Latitude": "35.284619",
             "Rating": "5"
         
         ] as [String: Any]
+        jaffa.setValue(jaffaData)
         
-        child.setValue(dataType)
+        
+        let fattoushData = [
+            "Restaurant Name": "Fattoush",
+            "Food Type": "Mediterranean",
+            "Longitute": "-120.6680136",
+            "Latitude": "35.2934011",
+            "Rating": "5"
+            
+            ] as [String: Any]
+        fattoush.setValue(fattoushData)
+        
+        
+        let pitaData = [
+            "Restaurant Name": "Pita Pit",
+            "Food Type": "Mediterranean",
+            "Longitute": "-120.67058800000001",
+            "Latitude": "35.2944202",
+            "Rating": "5"
+            
+            ] as [String: Any]
+        pitaPit.setValue(pitaData)
+        
+        
+        let petraData = [
+            "Restaurant Name": "Petra",
+            "Food Type": "Mediterranean",
+            "Longitute": "-120.65794870000002",
+            "Latitude": "35.2830522",
+            "Rating": "5"
+            
+            ] as [String: Any]
+        petra.setValue(petraData)
+        
+        
+        
       
         // Do any additional setup after loading the view, typically from a nib.
     }
